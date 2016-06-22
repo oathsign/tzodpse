e8d6daf9816008b36f88f806962ede328a457af8
誓言之印 <hhzgq@163.com>
2016-06-23 00:42:33 +0800

    Add 添加测试用例 检测junit是否能正常运行

---
+++ src/test/java/oathsign/HelloTest.java
@@ -0,0 +1,13 @@
+package oathsign;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloTest {
+
+    @Test
+    public void sayHello() {
+        assertEquals("Hello World!", "Hello World!");
+    }
+}
